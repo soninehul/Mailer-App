5ccb9d3feb6b5d7a2129aa532e20fcfc4c9b4812
Nehul Soni <soninehul@gmail.com>
2021-04-19 10:47:45 +0530

    Initial project

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const { Passport } = require('passport');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi : 'there' });
-});
+passport.use(new GoogleStrategy(
+    {
+        clientID : keys.googleClientID,
+        clientSecret : keys.googleClientSecret,
+        callbackURL : '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile:', profile);
+    }
+  )
+);
+
+app.get(
+    '/auth/google', passport.authenticate('google', {
+        scope : ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
